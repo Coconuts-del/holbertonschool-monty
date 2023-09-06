710c3520e5d6611b679fdb7bd40a0a990473bb78
Luckisback <jlucbilo@gmail.com>
2023-09-06 09:35:54 +0200

    monty.h file updated

--- monty.h
+++ monty.h
@@ -1,6 +1,13 @@
 #ifndef _MONTY_H_
 #define _MONTY_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <ctype.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -12,9 +19,9 @@
  */
 typedef struct stack_s
 {
-int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
 /**
@@ -27,12 +34,14 @@ int n;
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void pall(stack_t **stack, __attribute((unused)) unsigned int ligne);
+void push(stack_t **stack, unsigned int ligne);
+stack_t *add_node(stack_t **stack, int n);
+void check(char *cmd, stack_t **stack, unsigned int ligne);
 
 
-
-
-#endif
+#endif                                                                                                                                                                                                              ~                                          
