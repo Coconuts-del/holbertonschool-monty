80acba189b95a3d38e2e54c679c8c9d0b49ddf18
abdel <holbertonc21@gmail.com>
2023-09-06 04:37:55 -0700

    added swap function

--- functions2.c
+++ functions2.c
@@ -36,3 +36,26 @@ void pop(stack_t **stack, unsigned int ligne)
 		(*stack)->prev = NULL;
 	free(tmp);
 }
+
+/**
+ * swap - swaps the top two.
+ * @stack: To head of stack
+ * @ligne : line number.
+ */
+void swap(stack_t **stack, unsigned int ligne)
+{
+	int swapper;
+
+	if (*stack && (*stack)->next)
+	{
+		swapper = (*stack)->n;
+		(*stack)->n = (*stack)->next->n;
+		(*stack)->next->n = swapper;
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", ligne);
+		exit(EXIT_FAILURE);
+		return;
+	}
+}
